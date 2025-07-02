ca7d491380726edca32fcefb77813b81d14fa50b
Matt <yeamusic21@yahoo.com>
2025-07-01 21:57:06 -0400

    started stepping through positionwise feedforward

--- PositionWiseFeedForward_dissect_code.py
+++ PositionWiseFeedForward_dissect_code.py
@@ -19,3 +19,20 @@ print(testmod)
 ###------------------
 ### Step Through Code
 ###------------------
+
+fc1 = nn.Linear(d_model, d_ff)
+fc2 = nn.Linear(d_ff, d_model)
+relu = nn.ReLU()
+
+# Create some fake data with dimensions 3, 10, 10
+fake_data = np.random.rand(3, 10, 10)
+print("Fake data:")
+print(fake_data)
+list_data = fake_data.tolist()
+x = torch.tensor(list_data)
+
+step1 = fc1(x) # 10x10 * 10x20 gives 10x20
+
+print("Step1 results:")
+print(step1)
+print(step1.size())
