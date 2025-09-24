c4dd4f04c73f7c6afac60f8536007887ef043a82
Matt <yeamusic21@yahoo.com>
2025-09-24 09:15:26 -0400

    reviewed training logic

--- transformer_tutorial.py
+++ transformer_tutorial.py
@@ -177,6 +177,24 @@ transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, nu
 src_data = torch.randint(1, src_vocab_size, (64, max_seq_length))  # (batch_size, seq_length)
 tgt_data = torch.randint(1, tgt_vocab_size, (64, max_seq_length))  # (batch_size, seq_length)
 
+print("====================")
+print("Reviewing input data ...")
 print("src_data:")
 print(src_data)
 print(src_data.size())
+
+print("====================")
+print("Training ...")
+
+criterion = nn.CrossEntropyLoss(ignore_index=0)
+optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
+
+transformer.train()
+
+for epoch in range(100):
+    optimizer.zero_grad()
+    output = transformer(src_data, tgt_data[:, :-1])
+    loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 1:].contiguous().view(-1))
+    loss.backward()
+    optimizer.step()
+    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
